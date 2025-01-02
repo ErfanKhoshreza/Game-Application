fff61771010e5fce2836a8b3cf904bdc77e7ca01
Erfan Khoshreza <erfanbusy@gmail.com>
2025-01-02 17:32:39 +0330

    finalized entities

--- entity/game.go
+++ entity/game.go
@@ -11,5 +11,11 @@ type Player struct {
 	UserID  uint
 	GameID  uint
 	Score   uint
-	Answers []string
+	Answers []PlayerAnswer
+}
+type PlayerAnswer struct {
+	ID         uint
+	PlayerID   uint
+	QuestionID uint
+	Choice     PossibleAnswerChoice
 }
--- entity/question.go
+++ entity/question.go
@@ -4,17 +4,47 @@ type Question struct {
 	ID              uint
 	Question        string
 	PossibleAnswers []PossibleAnswer
-	CorrectAnswer   string
-	Difficulty      string
+	CorrectAnswerID uint
+	Difficulty      QuestionDifficulty
 	CategoryID      uint
 }
 type PossibleAnswer struct {
 	ID      uint
 	Content string
-	Choice  string
+	Choice  PossibleAnswerChoice
 }
 type Answer struct {
 	ID       uint
 	PlayerID uint
 	Question uint
+}
+type PossibleAnswerChoice uint8
+
+func (p PossibleAnswerChoice) IsValid() bool {
+	if p >= PossibleAnswerA && p <= PossibleAnswerD {
+		return true
+	}
+	return false
+}
+
+const (
+	PossibleAnswerA PossibleAnswerChoice = iota + 1
+	PossibleAnswerB
+	PossibleAnswerC
+	PossibleAnswerD
+)
+
+type QuestionDifficulty string
+
+const (
+	easyDifficulty   QuestionDifficulty = "easy"
+	mediumDifficulty QuestionDifficulty = "medium"
+	hardDifficulty   QuestionDifficulty = "hard"
+)
+
+func (d QuestionDifficulty) isValid() bool {
+	if d == easyDifficulty || d == mediumDifficulty || d == hardDifficulty {
+		return true
+	}
+	return false
 }
