6bc150da7be83b7ad2c80dcfe8b656dd4e0f9a89
Erfan Khoshreza <erfanbusy@gmail.com>
2025-01-06 20:10:59 +0330

    implemented user service and register

---
+++ service/user/service.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"Game-Application/entity"
+	"Game-Application/pkg/phonenumber"
+	"errors"
+)
+
+type Repository interface {
+	isPhoneNumberUnique(phoneNumber string) (bool, error)
+	Register(u entity.User) (entity.User, error)
+}
+type Service struct {
+	repo Repository
+}
+type RegisterRequest struct {
+	Name        string
+	PhoneNumber string
+}
+type RegisterRespond struct {
+	User entity.User
+}
+
+func (s Service) Register(req RegisterRequest) (RegisterRespond, error) {
+	//		Validate Phone Number
+	// TODO : We Should Verify Phone Number by Verfication Code
+	if phonenumber.IsValid(req.PhoneNumber) {
+		return RegisterRespond{}, errors.New("invalid phone")
+	}
+	// checkUniqueNess Phone number
+	if isUnique, err := s.repo.isPhoneNumberUnique(req.PhoneNumber); err != nil || !isUnique {
+		if err != nil {
+			return RegisterRespond{}, err
+		}
+		return RegisterRespond{}, errors.New("phone Number is not Unique")
+	}
+	//	 Validate Name
+	if len(req.Name) < 3 {
+		return RegisterRespond{}, errors.New("name is too short")
+	}
+
+	//	 create New User In  storage
+	user := entity.User{
+		Name:        req.Name,
+		PhoneNumber: req.PhoneNumber,
+		ID:          0,
+	}
+	registeredUser, err := s.repo.Register(user)
+	if err != nil {
+		return RegisterRespond{}, err
+	}
+	//	Retuurn creaTED User
+	return RegisterRespond{User: registeredUser}, nil
+}
