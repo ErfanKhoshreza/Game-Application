83669d8c7a026f4ba4a82ee3201ae35da6fd32c3
Erfan Khoshreza <erfanbusy@gmail.com>
2025-01-08 13:34:35 +0330

    implemented check phone number uniquness

--- repository/mongo/user.go
+++ repository/mongo/user.go
@@ -4,14 +4,25 @@ import (
 	"Game-Application/entity"
 	"Game-Application/pkg/password"
 	"context"
+	"errors"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
 
-//func (d DB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
-//	d.Database.Collection("gameUser").FindOne()
-//}
+func (d DB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	user := d.Database.Collection("gameUser").FindOne(ctx, bson.M{"phoneNumber": phoneNumber})
+	if errors.Is(user.Err(), mongo.ErrNoDocuments) {
+		return false, nil
+	} else if user.Err() != nil {
+		return false, user.Err()
+	}
+	return true, nil
+}
 
 func (d DB) Register(u entity.User) (entity.User, error) {
 	hashedPassword, err := password.HashPassword(u.Password)
@@ -27,7 +38,7 @@ func (d DB) Register(u entity.User) (entity.User, error) {
 	// Create a context with a timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	registeredUser, CErr := d.Database.Collection("gameUser").InsertOne(ctx, u)
+	registeredUser, CErr := d.Database.Collection("gameUser").InsertOne(ctx, user)
 	if CErr != nil {
 		return entity.User{}, err
 	}
